17719508683cc52922fa8eac7086dc902299a548
mikeart00 <mikeart00@gmail.com>
2015-10-31 19:51:12 -0600

    Conflictos resueltos commit gaby y mickey

--- Servidor.java
+++ Servidor.java
@@ -12,8 +12,12 @@ public class Servidor implements PublicacionSuscripcion {
     
     Hashtable<String, Usuario> usuarios = new Hashtable<String, Usuario>();
     Hashtable<String, Grupo> grupos = new Hashtable<String, Grupo>();
+    Hashtable<String, Usuario> anuncios = new Hashtable<String, Usuario>();
+    int anuncio_id;
     
-    public Servidor() {}
+    public Servidor() {
+      anuncio_id = 0;
+    }
 
     public boolean registrarse(String nombre_de_usuario) {
 
@@ -30,7 +34,7 @@ public class Servidor implements PublicacionSuscripcion {
     }
     
     public boolean suscripcion(String usuario, String topico) {
-        System.out.println("suscripcion: Petición de suscribir usuario:  "+usuario + " a grupo: " + topico);
+        System.out.println("suscripcion: Petición de suscribir usuario:  "+usuario + "a grupo: " + topico);
         
         //Verificar que el topico exista
         if(!grupos.containsKey(topico)) {
@@ -64,9 +68,10 @@ public class Servidor implements PublicacionSuscripcion {
         return u.leeNotificacion();
     }
             
-    public void publicaAnuncio(String usuario, String topico, String mensaje)
+    public void publicaAnuncio(String Usuario, String topico, String mensaje)
     {
        Grupo g; 
+	   Usuario u;
        //Existe el grupo indicado?
        if(!grupos.containsKey(topico)) {
             grupos.put(topico, new Grupo(topico));
@@ -76,10 +81,77 @@ public class Servidor implements PublicacionSuscripcion {
        //El grupo ya existe
        g = grupos.get(topico);
        System.out.println("Mandando anuncio "+mensaje+" a Grupo "+topico+" ");
+	   
+       //agrega el anuncio al pool de anuncios
+       ++anuncio_id;
+       u = usuarios.get(Usuario);
+       anuncios.put(Integer.toString(anuncio_id), u);
+       
        //Manda el anuncio
-       g.broadcast(new Notificacion(mensaje));
+       g.broadcast(new Notificacion(Usuario, mensaje, anuncio_id));
 
-    }        
+    }     
+
+public boolean hacerOferta(int num_mensaje, float precio)
+    {
+       Usuario u;
+       String msj = Integer.toString(num_mensaje);
+       System.out.println("HacerOferta - msj num: "+msj+"");
+      
+       //Encontrar si ese num de oferta esta registrado
+       if(!anuncios.containsKey(msj)) {
+         System.out.println("Anuncio no encontrado");
+         return false;
+       } 
+       
+       //Ya lo encontramos, ahora hay que obtener el usuario que lo mando.
+       u = anuncios.get(msj);
+       //Mandarle la oferta al usuario que publico el anuncio
+       Notificacion u2 = new Notificacion(u.nombre_de_usuario, num_mensaje, precio); 
+       u.agregaNotificacion(u2);
+       return false; 
+    }    
+    
+    public boolean venderProducto(String Usuario, int num_mensaje, float precio, boolean venta)
+    {
+       //Usuario es el usuario al que se le vendera el producto. 
+       String msj = Integer.toString(num_mensaje);
+       System.out.println("venderproducto - msj num: "+msj+"");
+       Notificacion n;
+       Usuario comprador;
+       
+       //Decidir si se hara la venta o no.
+       //Existe el usuario
+       if(!usuarios.containsKey(Usuario)) {
+         System.out.println("Usuario no encontrado"); 
+       }
+       
+       //Existe el anuncio?
+       if(!anuncios.containsKey(msj)) {
+         System.out.println("Anuncio no encontrado");
+         return false;
+       }
+                   
+       comprador = usuarios.get(Usuario);
+       
+       if(venta == true)
+       {
+         //Mandar notificacion de que si se realizara la venta  
+         n = new Notificacion(comprador.nombre_de_usuario, msj, true);
+         comprador.agregaNotificacion(n);
+         anuncios.remove(msj);
+         System.out.println("Compra completada");  
+       }
+       else 
+       { 
+         // mandar notificacion de que se rechazo la oferta  
+         n = new Notificacion(comprador.nombre_de_usuario, msj, false);
+         comprador.agregaNotificacion(n);
+       }
+       return true;
+
+    }
+	
     
     public static void main(String args[]) {
 
