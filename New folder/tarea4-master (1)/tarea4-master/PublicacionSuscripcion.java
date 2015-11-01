f03f235c2ce368412f5cbefcd4eb9fd4de21855b
mikeart00 <mikeart00@gmail.com>
2015-10-31 19:53:54 -0600

    Limpiando Folder de achivos viejos

--- New folder/tarea4-master (1)/tarea4-master/PublicacionSuscripcion.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface PublicacionSuscripcion extends Remote {
-    
-    void registrarse(String usuario) throws RemoteException;
-    boolean subscribirse(String topico) throws RemoteException;
-    boolean desubscribirse(String topico) throws RemoteException;
-}
--- New folder/tarea4-master (1)/tarea4-master/README.md
+++
@@ -1,2 +0,0 @@
-# tarea4
-Tarea 4. Sistemas Distribuidos
